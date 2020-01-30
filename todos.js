8ebb6284f07fad16704f13f564f73bdf5f1fc8a3
Yuri Alves Miguel <54409166+hyperyuri@users.noreply.github.com>
2020-01-30 08:08:19 -0400

    CSS

--- todos.js
+++ todos.js
@@ -1,15 +1,18 @@
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
+
 var buttonElement = document.querySelector('#app button');
 
 var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
-function renderTodos(){
-   listElement.innerHTML = '';
+function renderTodos() {
+    listElement.innerHTML = '';
 
-    for (todo of todos){
+    for (todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
+
+        var presseter = document.createAttribute
 
         var linkElement = document.createElement('a');
 
@@ -31,16 +34,33 @@ function renderTodos(){
 
 renderTodos();
 
-function addTodo(){
+function addTodo() {
     var todoText = inputElement.value;
 
-    todos.push(todoText);
-    inputElement.value = '';
-    renderTodos();
-    saveToStorage();
+    if (todos.length <12){
+        if (inputElement.value === '') {
+            alert("Campo Vazio");
+            saveToStorage();
+        }
+        else {
+            todos.push(todoText);
+            renderTodos();
+            saveToStorage();
+            inputElement.value = ''
+        }
+    }
+    else{
+        alert("Numero maximo de elementos. Nada será feito")
+    } 
+        
+    
 }
 
 buttonElement.onclick = addTodo;
+
+
+
+
 
 function deleteTodo(pos) {
     todos.splice(pos, 1);
